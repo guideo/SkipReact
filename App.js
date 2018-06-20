75fcd559187427023b85cf27aefe5f1b48d9de58
Guilherme Deo <guideo@gmail.com>
2018-06-19 23:28:08 -0300

    Home screen completed

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Icon } from 'react-native-elements';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
 export default class App extends React.Component {
   render() {
@@ -33,17 +34,73 @@ export default class App extends React.Component {
 			</View>
 			
 			<View style={{flex: 8, backgroundColor: 'white'}}>
+				<Restaurant name='Sogbu Restaurant' img='1' score='9.7'/>
+				<Restaurant name='Melting Pot Bar & Grill' img='2' score='9.3'/>
+				<Restaurant name='Premium Burger' img='3' score='9.6'/>
 			</View>
 		</View>
     );
   }
 }
 
+const RESTAURANT_LOGOS = {
+	logo1: require('./assets/resLogo1.jpg'),
+	logo2: require('./assets/resLogo2.jpg'),
+	logo3: require('./assets/resLogo3.jpg'),
+}
+
+class Restaurant extends React.Component {
+	
+	getImage(num: number) {
+		return RESTAURANT_LOGOS['logo' + num];
+	}
+	
+	render() {
+		return (
+			<View style={styles.restaurant}>
+				<View style={{flex: 2, justifyContent: 'center', alignItems: 'center'}}>
+					<Image
+						style={styles.logo}
+						source={this.getImage(this.props.img)}/>
+				</View>
+				<View style={{flex: 4.2, justifyContent: 'center', alignItems: 'flex-start'}}>
+					<Text style={{paddingBottom: 10, fontWeight: 'bold'}}>{this.props.name}</Text>
+					<Text style={{paddingBottom: 10}}>30 - 50 mins</Text>
+					<Text style={{color: '#bbbbbb'}}>$3.45</Text>
+				</View>
+				<View style={{flex: 1.3, alignItems: 'center'}}>
+					<View style={styles.score}>
+						<Text style={{color: 'white', fontWeight: 'bold', fontSize: 15}}>{this.props.score}</Text>
+					</View>
+				</View>
+			</View>
+			//<View>Hello {this.props.name}!</View>
+		);
+	}
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  restaurant: {
+	flex: 1,
+	flexDirection: 'row',
+	borderBottomColor: '#dddddd',
+	borderBottomWidth: 2
+  },
+  logo: {
+	borderRadius: 10,
+	borderWidth: 2,
+	borderColor: '#000000',
+	borderWidth: 2,
+	width: 80,
+	height: 80,
+  },
+  score: {
+	  marginTop: 15,
+	  alignItems: 'center',
+	  justifyContent: 'center',
+	  borderRadius: 5,
+	  backgroundColor: 'red',
+	  width: 40,
+	  height: 30,
   },
 });
