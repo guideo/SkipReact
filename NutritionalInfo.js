8a7e6810b42caac9fa11b5e4ce5025be5e633dba
Guilherme Deo <guideo@gmail.com>
2018-06-21 15:02:56 -0300

    Retrieving nutritional info for elements on the Ingredients List.

--- NutritionalInfo.js
+++ NutritionalInfo.js
@@ -8,26 +8,106 @@ export default class FoodInfo extends React.Component {
 	
 	constructor(props){
 		super(props);
-		this.state ={ isLoading: true }
+		this.state ={ isLoading: true,
+                      count: 0,
+                      dataSource: '',
+                      nOfIngredients: 1,
+                      ndbNos: [],
+                      info: [],
+                      foodFetched: false }
 	}
-
-	getNDBNO(url: string){
-		fetch(url)
+    
+    getNDBNO(){
+        const { navigation } = this.props;
+        var ingredients = navigation.getParam('ingredients', []);
+		this.fetchFoodNdb(ingredients);
+	}
+    
+    fetchFoodNdb(list){
+        let ndbBkup = [...this.state.ndbNos];
+        fetch(this.buildURLForFood(list[0].name))
 		  .then((response) => response.json())
 		  .then((responseJson) => {
-
+            ndbBkup.push({ndb: responseJson.list.item[0].ndbno, name: responseJson.list.item[0].name});
 			this.setState({
-				isLoading: false,
-				dataSource: responseJson.list.item,
+                dataSource: responseJson,
+                ndbNos: ndbBkup,
+                count: this.state.count + 1
 			}, function(){
-				
+                list.splice(0, 1);
+                if(list.length>0){
+                    this.fetchFoodNdb(list);
+                }
+                if(this.state.count >= this.state.nOfIngredients && !this.state.foodFetched){
+                    this.setState({ foodFetched: true }, function(){
+                        var ndbList = [...this.state.ndbNos];
+                        this.fetchFoodNutrients(ndbList);
+                    });
+                }
 			});
 
 		})
 		  .catch((error) =>{
 			console.error(error);
 		});
-	}
+    }
+    
+    fetchFoodNutrients(list){
+        let infoBkup = [...this.state.info];
+        fetch(this.buildURLForNutrients(list[0].ndb))
+		  .then((response) => response.json())
+		  .then((responseJson) => {
+            infoBkup.push({energy: responseJson.report.foods[0].nutrients[0].value,
+              sugar: responseJson.report.foods[0].nutrients[1].value,
+              fat: responseJson.report.foods[0].nutrients[2].value,
+              carbs: responseJson.report.foods[0].nutrients[3].value});
+			this.setState({
+                dataSource: responseJson,
+                info: infoBkup,
+                count: this.state.count + 1
+			}, function(){
+                list.splice(0, 1);
+                if(list.length>0){
+                    this.fetchFoodNutrients(list);
+                }
+                if(this.state.count >= this.state.nOfIngredients*2 && this.state.isLoading){
+                    this.setState({ isLoading: false });
+                }
+			});
+		})
+		  .catch((error) =>{
+			console.error(error);
+		});
+    }
+    
+    buildURLForFood(item){
+        const url = 'https://api.nal.usda.gov/ndb/search/?format=json&ds=Standard%20Reference&sort=r&max=1&offset=0&api_key=N8yGCm0uUt20Cvdo7rYKM4hxcPi3DLZ04dxE3kpB&q=';
+        let retURL = url + item;
+        console.log(retURL);
+        return retURL;
+    }
+    
+    buildURLForNutrients(item){
+        const url = 'https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=N8yGCm0uUt20Cvdo7rYKM4hxcPi3DLZ04dxE3kpB&nutrients=205&nutrients=204&nutrients=208&nutrients=269&ndbno=';
+        let retURL = url + item;
+        console.log(retURL);
+        return retURL;
+    }
+    
+    componentDidMount(){
+        const { navigation } = this.props;
+        var name = navigation.getParam('name', 'Undefined');
+        var ingredients = navigation.getParam('ingredients', []);
+        this.setState({ nOfIngredients: ingredients.length },  function() {
+                console.log("###############################");
+                console.log(ingredients.length.toString());
+                console.log("@@@@@@@@@@@@@@@@@@@@@@");
+                console.log(this.state.nOfIngredients);
+                this.getNDBNO();
+            });
+        
+        return;
+    }
 	
 	render() {
 		
@@ -35,7 +115,8 @@ export default class FoodInfo extends React.Component {
 		const name = navigation.getParam('name', 'Undefined');
 		const ingredients = navigation.getParam('ingredients', 'empty');
 		
-		this.getNDBNO('https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=r&ds=Standard%20Reference&max=1&offset=0&api_key=N8yGCm0uUt20Cvdo7rYKM4hxcPi3DLZ04dxE3kpB')
+		//this.getNDBNO('https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=r&ds=Standard%20Reference&max=1&offset=0&api_key=N8yGCm0uUt20Cvdo7rYKM4hxcPi3DLZ04dxE3kpB')
+        //this.getNDBNO('https://jsonplaceholder.typicode.com/posts')
 		
 		if(this.state.isLoading){
 			return(
@@ -48,9 +129,14 @@ export default class FoodInfo extends React.Component {
 		return(
 			<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
 				<Text>{ingredients.length.toString()}</Text>
+                <FlatList
+					data={this.state.ndbNos}
+					renderItem={({item}) => <Text>{item.ndb}, {item.name}</Text>}
+					keyExtractor={(item, index) => index}
+				/>
 				<FlatList
-					data={this.state.dataSource}
-					renderItem={({item}) => <Text>{item.name}, {item.ndbno}</Text>}
+					data={this.state.info}
+					renderItem={({item}) => <Text>{item.energy}, {item.sugar}, {item.fat}, {item.carbs}</Text>}
 					keyExtractor={(item, index) => index}
 				/>
 			</View>
